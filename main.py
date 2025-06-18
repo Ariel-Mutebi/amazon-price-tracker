14192ed7fa08e09b14f2c908298261c940c7f0f7
black-RAM <pplanes311@gmail.com>
2025-06-18 19:07:44 +0300

    Scrape product price complete

--- main.py
+++ main.py
@@ -14,4 +14,7 @@ response = requests.get(AMAZON_URL, headers=just_act_normal)
 response.raise_for_status()
 
 let_him_cook = BeautifulSoup(response.text, "html.parser")
-print(let_him_cook.prettify())
+dollars = let_him_cook.find(class_="a-price-whole").getText()
+cents = let_him_cook.find(class_="a-price-fraction").getText()
+price_floating_point = float(dollars + cents) # this addition is actually string concatenation
+print(price_floating_point)
