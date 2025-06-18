e74ecb7fe86cdcc620b1ca9e567a5b126d9c3e11
black-RAM <pplanes311@gmail.com>
2025-06-18 18:49:27 +0300

    Oh, that was my bad (just fixed my mistake of confusing params and headers)

--- main.py
+++ main.py
@@ -6,14 +6,12 @@ from bs4 import BeautifulSoup
 load_dotenv()
 AMAZON_URL = "https://a.co/d/ipL3f6L"
 
-# browser headings so Amazon won't notice that this is a bot.
 just_act_normal = {
     "User-Agent": os.environ.get("MY_USER_AGENT"),
     "Accept-Language": "en-US,en;q=0.9",
 }
-response = requests.get(AMAZON_URL, just_act_normal)
-response.raise_for_status() # Argh, status 500 again? Still getting ratted out!
+response = requests.get(AMAZON_URL, headers=just_act_normal)
+response.raise_for_status()
 
-# They just won't let bro cook, will they?
 let_him_cook = BeautifulSoup(response.text, "html.parser")
 print(let_him_cook.prettify())
